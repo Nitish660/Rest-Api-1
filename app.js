91c701315e8de8f7edfc6146789e7094701be9d8
Nitish660 <20051742@kiit.ac.in>
2023-12-20 15:08:23 +0530

    rest api

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./db/connect.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const PORT = process.env.PORT || 6001;
 
 const productsRoutes = require("./routes/products");
 
-/*app.get("/", (req, res) => {
-    res.send("Hi, I am live");
-});*/
-
 app.use("/", productsRoutes);
 
 const start = async () => {
     try {
+        await connectDB();
+      
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
